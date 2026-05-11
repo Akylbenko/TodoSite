4f14e822de95f46a87f145d6a0238fc612d49e4e
akylbenko2 <licenzeeya@gmail.com>
2026-05-12 02:42:18 +1000

    Fix API URL and CORS

--- todolist/frontend/src/App.js
+++ todolist/frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = 'https://todo-backend-khi2.onrender.com';
+const API_URL = 'https://todo-backend-khi2.onrender.com/api';
 
 function App() {
   const [tasks, setTasks] = useState([]);
